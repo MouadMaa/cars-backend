71d62388b33616b64c717eeec1e87c32b75b0dbc
mouadmaa <mouadmaaroufi00@gmail.com>
2022-07-14 15:15:43 +0100

    modify each response on cars controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -7,15 +7,18 @@ import {
   Param,
   Delete,
   Query,
+  UseInterceptors,
 } from '@nestjs/common'
 import { Car } from '@prisma/client'
 import { FilterQueryDto } from 'src/common/dto/filter-query.dto'
+import { ResponsesInterceptor } from 'src/common/interceptors/responses.interceptor'
 import { ParseObjectIdPipe } from 'src/common/pipes/parse-object-id.pipe'
 import { CarsService } from './cars.service'
 import { CreateCarDto } from './dto/create-car.dto'
 import { UpdateCarDto } from './dto/update-car.dto'
 
 @Controller('cars')
+@UseInterceptors(ResponsesInterceptor)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 

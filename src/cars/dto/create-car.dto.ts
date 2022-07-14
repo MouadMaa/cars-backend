2186a5220698e2bc2e576f63cafbeb8e876fe9f5
mouadmaa <mouadmaaroufi00@gmail.com>
2022-07-14 13:38:16 +0100

    Added some properties to car model

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,6 +1,27 @@
-import { IsString } from 'class-validator'
+import { IsNumber, IsOptional, IsString } from 'class-validator'
 
 export class CreateCarDto {
   @IsString()
   name: string
+
+  @IsString()
+  model: string
+
+  @IsString()
+  brand: string
+
+  @IsNumber()
+  price: number
+
+  @IsOptional()
+  @IsString()
+  desc?: string
+
+  @IsOptional()
+  @IsString()
+  color?: string
+
+  @IsOptional()
+  @IsString({ each: true })
+  images: string[]
 }
